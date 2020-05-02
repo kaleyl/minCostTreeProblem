a75d3e452c504ab116f8032b0a445c84584cb20f
ChingYi Leung <kkaleyleung@berkeley.edu>
2020-05-01 21:02:21 -0700

    mdcs

--- solver.py
+++ solver.py
@@ -26,22 +26,54 @@ def solve(G):
     elif numEdges == (numNodes- 1):
         T = solveTree(G)
     else : 
-        T = nx.minimum_spanning_tree(G)
+        T = solveGraph(G)
     return T
 
 
 def solveTree(G):
+    """ solve for tree 
+    - evaluating whether a leaf is worth adding to T
+    
+    """
     nonleaf = [v for v in G.nodes() if G.degree[v] > 1]
     T = G.subgraph(nonleaf).copy()
-    avg = T.size(weight='weight') / len(nonleaf)
+    count = len(nonleaf)
+    avg = T.size(weight = 'weight') / count
     leafs = [v for v in G.nodes() if G.degree[v] == 1]
     for v in leafs:
         u = G.neighbors(v)
         weight = G.get_edge_data(v, u, default=0)
         if weight < avg and weight != 0:
             T.add_node(v)
-            avg += weight
+            avg += weight / (count + 1)
+            count += 1
     return T
+
+def solveGraph(G):
+    # using dominating set to solve for general case
+    '''
+    Look for all ds and calculate the shortest path that connects
+    all the vertices in the ds.
+    return the ds with the shortest path.
+    '''
+    minSet = None
+    minDistance = G.number_of_nodes()
+    for v in G.nodes():
+        ds = nx.dominating_set(G, v)
+        temp = G.subgraph(ds).copy()
+        if nx.is_connected(temp) and len(temp) < minDistance:
+            minSet = ds
+            minDistance = len(temp)
+    if minSet == None:
+        return nx.minimum_spanning_tree(G, weight='weight')
+    T = nx.Graph()
+    for v in minSet:
+        T.add_node(v)
+    return T
+
+def getShortestPath(G, ds):
+    return
+
 
 # Usage: python3 solver.py inputs
 if __name__ == '__main__':
